a5fbd3ccd23d71cb5d6f01a639757e0f975d09b3
preethdev <123038428+preethdev@users.noreply.github.com>
2023-01-19 10:36:30 +0530

    Add files via upload

---
+++ src/pages/login/landingPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar";
+import SideNav from "../../components/SideNav";
+
+export default function LoginScreen() {
+  return (
+    <div className='h-screen overflow-y-clip'>
+      <Navbar/>
+      <div className="h-5/6">
+        
+      </div>
+    </div>
+  )
+}
